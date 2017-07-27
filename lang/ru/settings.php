b7f61e8da3c9fafb37fe49a49aaee9023287e955
Mykola Ostrovskyy <dwpforge@gmail.com>
2017-07-27 22:04:28 +0300

    Add Russian and Ukrainian translations

---
+++ lang/ru/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Russian language file
+ *
+ * @author Mykola Ostrovskyy <dwpforge@gmail.com>
+ */
+
+$lang['style']           = "Стиль";
+$lang['style_o_default'] = "Стандартный";
+$lang['style_o_symbols'] = "Символы";
+$lang['style_o_memes']   = "Мемы";
---
+++ lang/uk/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Ukrainian language file
+ *
+ * @author Mykola Ostrovskyy <dwpforge@gmail.com>
+ */
+
+$lang['style']           = "Стиль";
+$lang['style_o_default'] = "Стандартний";
+$lang['style_o_symbols'] = "Символи";
+$lang['style_o_memes']   = "Меми";
