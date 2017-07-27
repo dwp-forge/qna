f0a495834d123404febdf7d044af9a273ea5b82a
Mykola Ostrovskyy <dwpforge@gmail.com>
2017-07-27 22:03:44 +0300

    Explicitly specify plugin name for Configuration Manager

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -5,6 +5,8 @@
  * @author Moisés Braga Ribeiro <moisesbr@gmail.com>
  */
 
+$lang['plugin_settings_name'] = 'QnA';
+
 $lang['style']           = "Style";
 $lang['style_o_default'] = "Default";
 $lang['style_o_symbols'] = "Symbols";
